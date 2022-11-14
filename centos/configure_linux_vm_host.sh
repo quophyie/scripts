762ebae25b46e6283964fbc32999a9e6480731dc
quophyie <quophyie@yahoo.com>
2022-11-14 21:17:37 +0000

    Update configure_linux_vm_host.sh to now correctly set the default Ip of the host being configured
    Update messages with echos on separate lines
    
    call configure_user_shell for USER_UNDER_CONFIG and configure_user_shell  for "root"
    
    call install_zsh_and_oh_my_zsh for USER_UNDER_CONFIG and   install_zsh_and_oh_my_zsh  for "root"
    
    set CONFIGURE_DNS_CONFIRMATION to default of n
    
    call configure_networking as the last item to be configured

--- centos/configure_linux_vm_host.sh
+++ centos/configure_linux_vm_host.sh
@@ -3,27 +3,6 @@
 set -e
 source "${PWD}/shared_funcs.sh"
 
-## Configures /etc/resolv.conf
-#create_nameserver_config(){
-#  local resolv_conf=/etc/resolv.conf
-#  echo "Creating $resolv_conf ..."
-#  backup_file $resolv_conf
-#
-#  echo "# $HOSTNAME resolv.conf
-#
-#nameserver $NS_IP
-## Google name servers i.e. Google DNS Servers
-#nameserver 8.8.8.8
-#nameserver 8.8.1.1
-#
-## Cloudflare name servers i.e Cloudflare DNS Servers
-##nameserver 1.1.1.1
-#" > $resolv_conf
-#
-#  echo "Finished creating $resolv_conf"
-#}
-#
-
 
 # HOSTNAME
 # NIC
@@ -64,6 +43,7 @@ configure_user_provided_input_and_initialise_vars() {
           fi
       done
 
+  add_empty_line
 
   ip a
   add_empty_line
@@ -76,75 +56,89 @@ configure_user_provided_input_and_initialise_vars() {
             fi
         done
 
-  echo "Please provide the name of the host you are configuring
-Default: $(hostname)"
+  add_empty_line
+  local defaultNicIp
+  get_ip "${NIC}" defaultNicIp
+  echo "Please enter the  IP address of the NIC (Network Interface Card) $NIC"
+  echo "Default: ${defaultNicIp}"
+  read NIC_IP
+  NIC_IP=${NIC_IP:-${defaultNicIp}}
+
+  add_empty_line
+  echo "Please provide the name of the host you are configuring"
+  echo "Default: $(hostname)"
   read HOSTNAME
   HOSTNAME=${HOSTNAME:-$(hostname)}
 
-   echo "Please provide the IP of the nameserver
-Default: 192.168.0.2"
-   read NS_IP
-   NS_IP=${NS_IP:-"192.168.0.2"}
+  add_empty_line
+  echo "Please provide the IP of the nameserver"
+  echo "Default: 192.168.0.2"
+  read NS_IP
+  NS_IP=${NS_IP:-"192.168.0.2"}
 
-  echo "Please provide the name of the nameserver
-Default: mainframe"
+  add_empty_line
+  echo "Please provide the name of the nameserver"
+  echo "Default: mainframe"
   read NS_NAME
   NS_NAME=${NS_NAME:-mainframe}
 
-  echo "Please provide the domain of the nameserver
-Default: homelan.com"
+  add_empty_line
+  echo "Please provide the domain of the nameserver"
+  echo "Default: homelan.com"
   read NS_DOMAIN_NAME
   NS_DOMAIN_NAME=${NS_DOMAIN_NAME:-homelan.com}
 
-  echo "Do you want to configure this host to dynamically update the DNS server $NS_NAME with the ip address and name of this host i.e. $HOSTNAME?
-Default: y"
+  add_empty_line
+  echo "Do you want to configure this host to dynamically update the DNS server $NS_NAME with the ip address and name of this host i.e. $HOSTNAME?"
+  echo "Default: y"
   print_confirmation_instructions
   read CONFIRM_CONFIGURE_DDNS_UPDATES
   CONFIRM_CONFIGURE_DDNS_UPDATES=${CONFIRM_CONFIGURE_DDNS_UPDATES:-y}
 
+  add_empty_line
   if  is_true "$CONFIRM_CONFIGURE_DDNS_UPDATES" ; then
-     echo "Do you want to try and copy the dynamic DNS update key from the DNS server (i.e $NS_NAME)
-Default: y"
+    echo "Do you want to try and copy the dynamic DNS update key from the DNS server (i.e $NS_NAME)"
+    echo "Default: y"
 
-     print_confirmation_instructions
-     read TRY_COPY_DDNS_UPDATE_KEY
-     TRY_COPY_DDNS_UPDATE_KEY=${TRY_COPY_DDNS_UPDATE_KEY:-y}
+    print_confirmation_instructions
+    read TRY_COPY_DDNS_UPDATE_KEY
+    TRY_COPY_DDNS_UPDATE_KEY=${TRY_COPY_DDNS_UPDATE_KEY:-y}
 
-     if is_true $TRY_COPY_DDNS_UPDATE_KEY ; then
-       echo "Please provide the username used to copy the dynamic DNS key from the DNS server i.e the user used to access $NS_NAME"
-       read NS_USER
+    if is_true $TRY_COPY_DDNS_UPDATE_KEY ; then
+      echo "Please provide the username used to copy the dynamic DNS key from the DNS server i.e the user used to access $NS_NAME"
+      read NS_USER
 
-          while [ -z $NS_USER ]
-               do
-                   read NS_USER
-                   if [ -z $NS_USER ]; then
-                       echo "The DNS server user is required"
-                   fi
-               done
-
-       echo "Please provide the password of user $NS_USER "
+      while [ -z $NS_USER ]
+      do
+        read NS_USER
+        if [ -z $NS_USER ]; then
+          echo "The DNS server user is required"
+        fi
+       done
+      add_empty_line
+      echo "Please provide the password of user $NS_USER "
        # Dont echo password
-       read -s NS_PASS
+      read -s NS_PASS
 
-          while [ -z $NS_PASS ]
-               do
-                   # Dont echo password
-                   read -s NS_PASS
-                   if [ -z $NS_PASS ]; then
-                       echo "The password for DNS server user $NS_USER is required"
-                   fi
-               done
+      while [ -z $NS_PASS ]
+      do
+        # Dont echo password
+        read -s NS_PASS
+        if [ -z $NS_PASS ]; then
+          echo "The password for DNS server user $NS_USER is required"
+        fi
+      done
+      add_empty_line
+      echo "Please provide the full path to the dynamic DNS update key on the DNS server where the DNS Key should be copied from i.e $NS_NAME"
+      echo "Default: /etc/named/ddnsupdate.key"
+      read DDNS_UPDATE_KEY_ON_NS_SERVER
+      DDNS_UPDATE_KEY_ON_NS_SERVER=${DDNS_UPDATE_KEY_ON_NS_SERVER:-/etc/named/ddnsupdate.key}
+      add_empty_line
+      echo "Please provide the full file path to location where the dynamic update key will be stored on this host i.e $HOSTNAME"
+      echo "Default: /etc/named/ddnsupdate.key"
 
-       echo "Please provide the full path to the dynamic DNS update key on the DNS server where the DNS Key should be copied from i.e $NS_NAME
-Default: /etc/named/ddnsupdate.key"
-       read DDNS_UPDATE_KEY_ON_NS_SERVER
-    DDNS_UPDATE_KEY_ON_NS_SERVER=${DDNS_UPDATE_KEY_ON_NS_SERVER:-/etc/named/ddnsupdate.key}
-
-       echo "Please provide the full file path to location where the dynamic update key will be stored on this host i.e $HOSTNAME
-Default: /etc/named/ddnsupdate.key"
-
-       read DDNS_UPDATE_KEY
-       DDNS_UPDATE_KEY=${DDNS_UPDATE_KEY:-/etc/named/ddnsupdate.key}
+      read DDNS_UPDATE_KEY
+      DDNS_UPDATE_KEY=${DDNS_UPDATE_KEY:-/etc/named/ddnsupdate.key}
      else
        echo "*******************"
        echo "As you have opted not try to obtain the dynamic DNS update key from the DNS server, please make sure that there is a valid dynamic dns update key file at location /etc/named/ddnsupdate.key otherwise dynamic DNS updates will not work!!!!"
@@ -163,14 +157,19 @@ main() {
   install_git
   add_empty_line
   install_zsh_and_oh_my_zsh "$USER_UNDER_CONFIG" "$USER_UNDER_CONFIG_HOME" "$VCONSOLE_CONF"
+  add_empty_line
   install_zsh_and_oh_my_zsh "root" "/root" "$VCONSOLE_CONF"
 
-  configure_networking "$NIC" "$HOSTNAME" --local_nameserver_ip="${NS_IP}" --local_dns_domain_name="${NS_DOMAIN_NAME}"
   configure_user_shell "$USER_UNDER_CONFIG"
+  add_empty_line
   configure_user_shell "root"
+  add_empty_line
   if is_true "$CONFIRM_CONFIGURE_DDNS_UPDATES"; then
     configure_dynamic_dns_client "$NS_IP" "$NS_NAME" "$NS_DOMAIN_NAME" "$NIC" "$TRY_COPY_DDNS_UPDATE_KEY" "$NS_USER" "$NS_PASS" "$DDNS_UPDATE_KEY" "$DDNS_UPDATE_KEY_ON_NS_SERVER"
   fi
+
+  add_empty_line
+  configure_networking "$NIC" "$HOSTNAME" --nic_ip="${NIC_IP}" --local_nameserver_ip="${NS_IP}" --local_dns_domain_name="${NS_DOMAIN_NAME}"
 
   add_empty_line
   print_system_setup_completion_message 1
